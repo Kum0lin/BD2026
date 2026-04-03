5230f5c663e09893e533735550cf969b78f95ecf
kum0lin <dmitrijs.komolins@edu.rtu.lv>
2026-04-03 12:02:34 +0300

    Added 3 microservices, Booking-service, Hotel-service, User-service, Monolith app migrated in to the sepparated directory

--- src/main/java/com/booking/repository/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.booking.repository;
-
-import com.booking.model.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-
-}
--- src/main/java/com/booking/repository/HotelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.booking.repository;
-
-import com.booking.model.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-
-}
