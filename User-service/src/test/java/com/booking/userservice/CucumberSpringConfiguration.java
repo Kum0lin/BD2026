0070979be8e4ff1907a8922e833c1f4363c36b32
kum0lin <dmitrijs.komolins@edu.rtu.lv>
2026-05-25 12:04:48 +0300

    Add Cucumber contract tests

---
+++ User-service/src/test/java/com/booking/userservice/CucumberSpringConfiguration.java
@@ -0,0 +1,16 @@
+package com.booking.userservice;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@CucumberContextConfiguration
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:user-cucumber-contract-test;DB_CLOSE_DELAY=-1",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
+class CucumberSpringConfiguration {
+}
